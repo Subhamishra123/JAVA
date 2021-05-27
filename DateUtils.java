f21af4c1faf1780a96206ee0f782780ea04ef292
Subhasish Mishra <mishrasubhasis1996@gmail.com>
2021-05-27 20:31:50 +0530

    Bug-Fix

--- DateUtils.java
+++ DateUtils.java
@@ -2,6 +2,7 @@
 
 class DateUtils {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+int j=0;		
     }
 }
--- Temp.java
+++ Temp.java
@@ -1,5 +1,8 @@
 // Your First Program
 
 class Temp{
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+int j=0;		
+    }
 }
